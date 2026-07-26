88ed891e67487cf3f7dd48954355026048aed429
ezzebainer <ezzebainer@gmail.com>
2026-07-26 18:09:31 -0300

    Expandiendo el menu

--- js/productos.js
+++ js/productos.js
@@ -99,6 +99,48 @@ const productos = [
         ["vegano", "sinTacc"]
     ),
 
+    new Producto(
+        18,
+        "Americano",
+        "Espresso suave con agua caliente.",
+        3900,
+        "Cafés",
+        "img/Cafes/americano.jpg",
+        false,
+        ["vegano", "sinTacc"]
+    ),
+
+    new Producto(
+        19,
+        "Mocha",
+        "Espresso con chocolate y leche vaporizada.",
+        4900,
+        "Cafés",
+        "img/Cafes/mocha.jpg",
+        true,
+        ["sinLactosa"]
+    ),
+
+    new Producto(
+        20,
+        "Caramel Latte",
+        "Latte con salsa de caramelo artesanal.",
+        5200,
+        "Cafés",
+        "img/Cafes/caramelLatte.jpg",
+        false,
+        ["sinLactosa"]
+    ),
+
+    new Producto(
+        21,
+        "Cortado",
+        "Espresso con una pequeña porción de leche.",
+        3800,
+        "Cafés",
+        "img/Cafes/cortado.jpg"
+    ),
+
     // ---------- PASTELERÍA ----------
 
     new Producto(
@@ -126,6 +168,46 @@ const productos = [
         4500,
         "Pastelería",
         "img/Pasteleria/brownieNuez.jpg"
+    ),
+
+    new Producto(
+        22,
+        "Lemon Pie",
+        "Base crocante con crema de limón y merengue.",
+        5600,
+        "Pastelería",
+        "img/Pasteleria/lemonPie.jpg"
+    ),
+
+    new Producto(
+        23,
+        "Muffin de Arándanos",
+        "Muffin esponjoso con arándanos frescos.",
+        3800,
+        "Pastelería",
+        "img/Pasteleria/muffinArandanos.jpg"
+    ),
+
+    new Producto(
+        24,
+        "Budín de Banana",
+        "Budín casero con bananas maduras.",
+        3600,
+        "Pastelería",
+        "img/Pasteleria/budinBanana.jpg",
+        false,
+        ["vegano"]
+    ),
+
+    new Producto(
+        25,
+        "Alfajor de Maicena",
+        "Relleno de dulce de leche y coco rallado.",
+        2900,
+        "Pastelería",
+        "img/Pasteleria/alfajorMaicena.jpg",
+        false,
+        ["sinTacc"]
     ),
 
     // ---------- BRUNCH ----------
@@ -157,6 +239,46 @@ const productos = [
         "img/Brunchs/huevosBenedictinos.jpg"
     ),
 
+    new Producto(
+        26,
+        "Wrap Vegetariano",
+        "Vegetales grillados, hummus y hojas verdes.",
+        7200,
+        "Brunch",
+        "img/Brunchs/wrapVege.jpg",
+        false,
+        ["vegano"]
+    ),
+
+    new Producto(
+        27,
+        "Tostadas Francesas",
+        "Pan brioche con frutas y miel.",
+        6800,
+        "Brunch",
+        "img/Brunchs/tostadasFrancesas.jpg"
+    ),
+
+    new Producto(
+        28,
+        "Yogur con Granola",
+        "Yogur natural con granola artesanal y frutas.",
+        5200,
+        "Brunch",
+        "img/Brunchs/yogurGranola.jpg",
+        false,
+        ["sinTacc"]
+    ),
+
+    new Producto(
+        29,
+        "Pancakes",
+        "Pancakes con maple y frutos rojos.",
+        7400,
+        "Brunch",
+        "img/Brunchs/pancakes.jpg"
+    ),
+
     // ---------- INFUSIONES ----------
 
     new Producto(
@@ -177,6 +299,39 @@ const productos = [
         3900,
         "Infusiones",
         "img/Infusiones/teVerde.jpg",
+        false,
+        ["vegano", "sinTacc"]
+    ),
+
+    new Producto(
+        30,
+        "Matcha Latte",
+        "Matcha premium con leche espumada.",
+        4900,
+        "Infusiones",
+        "img/Infusiones/matchaLatte.jpg",
+        true,
+        ["sinLactosa"]
+    ),
+
+    new Producto(
+        31,
+        "Earl Grey",
+        "Té negro con esencia de bergamota.",
+        4100,
+        "Infusiones",
+        "img/Infusiones/earlGrey.jpg",
+        false,
+        ["vegano", "sinTacc"]
+    ),
+
+    new Producto(
+        32,
+        "Manzanilla",
+        "Infusión relajante de flores de manzanilla.",
+        3500,
+        "Infusiones",
+        "img/Infusiones/teManzanilla.jpg",
         false,
         ["vegano", "sinTacc"]
     ),
@@ -214,6 +369,48 @@ const productos = [
         "img/Bebidas/jugoRojo.jpg",
         false,
         ["vegano", "sinTacc"]
+    ),
+
+    new Producto(
+        33,
+        "Smoothie Tropical",
+        "Mango, ananá y banana licuados.",
+        5600,
+        "Bebidas",
+        "img/Bebidas/smoothieTropical.jpg",
+        false,
+        ["vegano", "sinTacc"]
+    ),
+
+    new Producto(
+        34,
+        "Chocolate Caliente",
+        "Chocolate belga caliente con crema.",
+        5200,
+        "Bebidas",
+        "img/Bebidas/chocolateCaliente.jpg"
+    ),
+
+    new Producto(
+        35,
+        "Iced Tea",
+        "Té frío con limón y menta.",
+        4300,
+        "Bebidas",
+        "img/Bebidas/icedTea.jpg",
+        false,
+        ["vegano", "sinTacc"]
+    ),
+
+    new Producto(
+        36,
+        "Smoothie de Frutilla",
+        "Frutillas naturales con yogur y hielo.",
+        5700,
+        "Bebidas",
+        "img/Bebidas/smoothieFrutilla.jpg",
+        false,
+        ["sinLactosa"]
     )
 
 ];
